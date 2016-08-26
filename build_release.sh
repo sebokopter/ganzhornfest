057086312d9b831a374af8d88073ac84a2e8f9a8
Sebastian Heil <mail+git@sebastianheil.de>
2016-08-26 14:34:14 +0200

    Prepare building v0.2.0.

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,14 @@
 version=$(awk 'BEGIN{FS="\""}; NR==2 { print $4 }' config.xml)
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
-/home/sebo/projekte/android-sdk-linux/build-tools/22.0.1/zipalign -fv 4 platforms/android/build/outputs/apk/android-release-unsigned.apk /home/sebo/projekte/ganzhornfest/platforms/android/build/outputs/apk/ganzhornfest.apk
-cp /home/sebo/projekte/ganzhornfest/platforms/android/build/outputs/apk/ganzhornfest.apk /home/sebo/projekte/ganzhornfest/platforms/android/build/outputs/apk/ganzhornfest-v${version}.apk
+# alternative:
+# ionic build --release android
+
+# not sure if there are newer alogirthms support, so we're using the defaults
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
+jarsigner -verbose -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
+
+# we have to always adjust the build-tools version
+#/home/sebo/projekte/android-sdk-linux/build-tools/22.0.1/zipalign -fv 4 platforms/android/build/outputs/apk/android-release-unsigned.apk /home/sebo/projekte/ganzhornfest/platforms/android/build/outputs/apk/ganzhornfest.apk
+/home/sebo/projekte/android-sdk-linux/build-tools/23.0.3/zipalign -fv 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/ganzhornfest.apk
+
+cp platforms/android/build/outputs/apk/ganzhornfest.apk platforms/android/build/outputs/apk/ganzhornfest-v${version}.apk
