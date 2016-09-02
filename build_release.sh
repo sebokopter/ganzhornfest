536a5572c0bf6bfa27e84f66ac46deeac4b5d83b
Sebastian Heil <mail+git@sebastianheil.de>
2016-09-02 09:09:03 +0200

    Sign apk with SHA1 again.

--- build_release.sh
+++ build_release.sh
@@ -3,9 +3,8 @@ cordova build --release android
 # alternative:
 # ionic build --release android
 
-# not sure if there are newer alogirthms support, so we're using the defaults
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
-jarsigner -verbose -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
+# don't use the defaults, some older devices don't support >SHA1 and >SHA1withRSA
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ganzhornfest-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ganzhornfest
 
 # we have to always adjust the build-tools version
 #/home/sebo/projekte/android-sdk-linux/build-tools/22.0.1/zipalign -fv 4 platforms/android/build/outputs/apk/android-release-unsigned.apk /home/sebo/projekte/ganzhornfest/platforms/android/build/outputs/apk/ganzhornfest.apk
